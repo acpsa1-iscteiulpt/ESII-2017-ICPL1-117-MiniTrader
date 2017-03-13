3ac4f1c110a09ce01abb51ecc129df714dc55f1a
Alexandre Silva <acpsa1@iscte-iul.pt>
2017-03-13 23:05:18 +0000

    Commit de teste

--- src/main/java/mt/client/impl/MicroTraderClientImpl.java
+++ src/main/java/mt/client/impl/MicroTraderClientImpl.java
@@ -10,8 +10,6 @@ import mt.comm.ClientComm;
 import mt.comm.impl.ClientCommImpl;
 
 public class MicroTraderClientImpl implements MicroTraderClient {
-	//comentário de teste
-	//comentário de teste para o vídeo
 	public static void main(String args[]) {
 		ClientComm clientComm = new ClientCommImpl();
 		MicroTraderClient client = new MicroTraderClientImpl();
