8169b4f3a35af6a0537a6883f3c56ba60c73703b
Alexandre Silva <acpsa1@iscte-iul.pt>
2017-05-02 16:08:12 +0100

    Implementação da business rule 3 comum aos 3 produtos.

--- src/main/java/mt/server/MicroServer.java
+++ src/main/java/mt/server/MicroServer.java
@@ -120,8 +120,8 @@ public class MicroServer implements MicroTraderServer {
 					verifyUserConnected(msg);
 
 					if (msg.getOrder().getNumberOfUnits() < 10) {
-						String aviso3 = "Não podes fazer um pedido com quantidade inferior a 10 unidades.";
-						JOptionPane.showMessageDialog(null, aviso3);
+						String aviso1 = "Não podes fazer um pedido com quantidade inferior a 10 unidades.";
+						JOptionPane.showMessageDialog(null, aviso1);
 					} else {
 						if (msg.getOrder().getServerOrderID() == EMPTY) {
 							msg.getOrder().setServerOrderID(id++);
