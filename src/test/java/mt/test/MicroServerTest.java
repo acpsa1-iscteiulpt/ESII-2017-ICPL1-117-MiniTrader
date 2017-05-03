a23cf87f3b0facf55f3442e8020870a9c0791d94
Alexandre Silva <acpsa1@iscte-iul.pt>
2017-05-03 16:43:38 +0100

    Mais algumas alterações aos testes.

--- src/test/java/mt/test/MicroServerTest.java
+++ src/test/java/mt/test/MicroServerTest.java
@@ -123,12 +123,10 @@ public class MicroServerTest {
 
 	@Test
 	public void testStart() throws Exception {
-		when(serverComm.getNextMessage()).thenReturn(msg1).thenReturn(msg2).thenReturn(msg3).thenReturn(msg4).thenReturn(msg5).thenReturn(msg6).thenReturn(null);
-		
+		when(serverComm.getNextMessage()).thenReturn(msg1).thenReturn(msg2).thenReturn(msg3).thenReturn(msg4).thenReturn(msg5).thenReturn(msg6).thenReturn(null);		
 		ms.start(serverComm);
-		System.out.println("É aqui?");
-		verify(serverComm, atLeastOnce()).sendOrder("userB", Order.createSellOrder("userA", "MSFT", 10, 20.0) );
-		verify(serverComm, atLeastOnce()).sendOrder("userB", Order.createBuyOrder("userB", "MSFT", 10, 21.0) );
+		verify(serverComm, atLeastOnce()).sendOrder("userB", Order.createSellOrder("userA", "MSFT", 0, 20.0) );
+		verify(serverComm, atLeastOnce()).sendOrder("userB", Order.createBuyOrder("userB", "MSFT", 0, 21.0) );
 	}
 	
 	@Test
@@ -172,7 +170,7 @@ public class MicroServerTest {
 		when(serverComm.getNextMessage()).thenReturn(msg1).thenReturn(msg2).thenReturn(msg3).thenReturn(msg4).thenReturn(msg5).thenReturn(msg6).thenReturn(null);
 		ms.start(serverComm);
 		
-		verify(serverComm, atLeastOnce()).sendOrder("userA", Order.createSellOrder("userA", "MSFT", 5, 20.0));
+		verify(serverComm, atLeastOnce()).sendOrder("userA", Order.createSellOrder("userA", "MSFT", 0, 20.0));
 	}
 	
 	@Test
